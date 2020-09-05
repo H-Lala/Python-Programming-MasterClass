ce8b5a684df0ad14e8cc1c8ab53b19baf737a4c5
H-Lala <hajiyeva.lalah@gmail.com>
2020-09-05 21:02:40 +0400

    -- strings , escape character

---
+++ Intro/escapeChar.py
@@ -0,0 +1,25 @@
+splitString = "This string has been\nsplit over\nseveral\ntimes"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('Single quotes " and \'e\'s testing some stuff".')
+
+print("Double quotes \" and 'e's testing some stuff\".")
+
+anotherSplitStriing = """This string
+has been \
+split over
+several
+times"""
+
+
+print(anotherSplitStriing)
+# if you don't start new lines in triple quotes use \
+
+# using of \ in the string
+
+print("C:\\Users\\admin\\text.txt")
+# or
+print(r"C:\Users\admin\text.txt")
---
+++ Intro/strings.py
@@ -0,0 +1,7 @@
+print("to learn Python")
+print('Python is interesting')
+greeting = "Hello"
+name = input("Please enter your name")
+print(greeting + name)
+# if I want a space , I can add
+print(greeting + ' ' + name)
